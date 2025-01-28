7a5e38b2ae768abfc44fde173387df1b57b54114
Kevin Yoga Pratama <ygkevin4@gmail.com>
2025-01-28 22:00:49 +0700

    Revisi Laporan

--- application/views/admin/filter_laporan.php
+++ application/views/admin/filter_laporan.php
@@ -1,25 +1,30 @@
 <div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Filter Laporan Transaksi</h1>
-        </div>
-    </section>
+<section class="section">
+	<div class="section-header">
+		<h1>Filter Laporan Transaksi</h1>
+	</div>
+</section>
 
-    <form method="POST" action="<?php echo base_url('admin/laporan') ?>">
-    	
-    	<div class="form-group">
-    		<label>Dari Tanggal</label>
-    		<input type="date" name="dari" class="form-control"></input>
-    		<?php echo form_error('dari','<span class="text-small text-danger">','</span>')  ?>
-    	</div>
-
-    	<div class="form-group">
-    		<label>Sampai Tanggal</label>
-    		<input type="date" name="sampai" class="form-control"></input>
-    		<?php echo form_error('sampai','<span class="text-small text-danger">','</span>')  ?>
-    	</div>
-
-    	<button type="submit" class="btn btn-primary"><i class="fas fa-eye"></i> Tampilkan Data</button>
+<form method="POST" action="<?php echo base_url('admin/laporan') ?>">
+	<div class="col-lg-12">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="form-group">
+						<label>Dari Tanggal</label>
+						<input type="date" name="dari" class="form-control"></input>
+						<?php echo form_error('dari','<span class="text-small text-danger">','</span>')  ?>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="form-group">
+						<label>Sampai Tanggal</label>
+						<input type="date" name="sampai" class="form-control"></input>
+						<?php echo form_error('sampai','<span class="text-small text-danger">','</span>')  ?>
+					</div>
+				</div>
+			</div>
+	</div>
+<button type="submit" class="btn btn-primary"><i class="fas fa-eye"></i> Tampilkan Data</button>
 
     </form>
 </div>
